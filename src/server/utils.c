c9df25aa93d38d39c0e843493fb14b420e1c7e97
Pedro <pedro.co.dev.ac@gmail.com>
2026-07-01 15:12:14 -0400

    melhorei o utils.c, evitando código duplicado e adicionei pagina 404 page not found

--- src/server/utils.c
+++ src/server/utils.c
@@ -34,20 +34,9 @@ static const char *get_mime(const char *url) {
   return "text/plain";
 }
 
-enum MHD_Result serve_file(struct MHD_Connection *connection,
-                           const char *path) {
-  FILE *fp = fopen(path, "rb");
-  if (!fp) {
-    const char *page = "<html><body><h1>404 Not Found</h1></body></html>";
-    struct MHD_Response *response = MHD_create_response_from_buffer(
-        strlen(page), (void *)page, MHD_RESPMEM_PERSISTENT);
-    MHD_add_response_header(response, "Content-Type", "text/html");
-    enum MHD_Result ret =
-        MHD_queue_response(connection, MHD_HTTP_NOT_FOUND, response);
-    MHD_destroy_response(response);
-    return ret;
-  }
-
+static enum MHD_Result send_file(struct MHD_Connection *connection,
+                                 FILE *fp, const char *mime,
+                                 enum MHD_Result status) {
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   fseek(fp, 0, SEEK_SET);
@@ -63,9 +52,29 @@ enum MHD_Result serve_file(struct MHD_Connection *connection,
 
   struct MHD_Response *response =
       MHD_create_response_from_buffer(size, buffer, MHD_RESPMEM_MUST_FREE);
-  MHD_add_response_header(response, "Content-Type", get_mime(path));
+  MHD_add_response_header(response, "Content-Type", mime);
 
-  enum MHD_Result ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+  enum MHD_Result ret = MHD_queue_response(connection, status, response);
+  MHD_destroy_response(response);
+  return ret;
+}
+
+enum MHD_Result serve_file(struct MHD_Connection *connection,
+                           const char *path) {
+  FILE *fp = fopen(path, "rb");
+  if (fp)
+    return send_file(connection, fp, get_mime(path), MHD_HTTP_OK);
+
+  FILE *nf = fopen("www/src/notfound.html", "rb");
+  if (nf)
+    return send_file(connection, nf, "text/html", MHD_HTTP_NOT_FOUND);
+
+  const char *page = "<html><body><h1>404 Not Found</h1></body></html>";
+  struct MHD_Response *response = MHD_create_response_from_buffer(
+      strlen(page), (void *)page, MHD_RESPMEM_PERSISTENT);
+  MHD_add_response_header(response, "Content-Type", "text/html");
+  enum MHD_Result ret =
+      MHD_queue_response(connection, MHD_HTTP_NOT_FOUND, response);
   MHD_destroy_response(response);
   return ret;
 }
